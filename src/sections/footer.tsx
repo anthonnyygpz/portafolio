04b53d790a6c2ffa7b6acb547526b089a8105390
anthonyygpz@gmail.com <anthonyygpz@gmail.com>
2025-03-24 00:42:27 -0700

    refactor(footer.tsx): agrego usuario

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -2,10 +2,10 @@ export const Footer = () => {
   return (
     <footer className="py-8 bg-muted/50 border-t">
       <div className="container mx-auto px-4 text-center">
-        <p className="text-muted-foreground">
-          © {new Date().getFullYear()} TuNombre. Todos los derechos reservados.
+        <p className="text-brown-500">
+          © {new Date().getFullYear()} anthonyygpz. Todos los derechos
+          reservados.
         </p>
-        <p className="text-sm text-muted-foreground mt-2"></p>
       </div>
     </footer>
   );
