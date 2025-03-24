e48cf144b62545c608233d6a8aa5f11182ca90dd
anthonyygpz@gmail.com <anthonyygpz@gmail.com>
2025-03-24 00:39:41 -0700

    feat(aboutMe.tsx): agrego el aboutMe
    Esto habla sobre mi y cosas que me apasionan

---
+++ src/sections/aboutMe.tsx
@@ -0,0 +1,28 @@
+const AboutMe = () => {
+  return (
+    <section className="text-white" id="about-me">
+      <div className="w-full max-w-4xl mx-auto py-16 px-4">
+        <h2 className="text-4xl font-bold text-center mb-12">Sobre mi</h2>
+
+        <p className="text-xl">
+          Soy desarrollador web apasionado con 1 año de experiencia en la
+          creacion de aplicaciones web modernas. Me espezializo en React y
+          FastAPI, y disfruto resolviendo problemas complejos con soluciones
+          elegantes.
+          <br />
+          Cuando no estoy programando, me gusta explorar nuevas tecnologias,
+          jugar videojuegos, salir a conocer el paisaje y tomar un respiro.
+          Tambien soy un entusiasta de la fisica y quimica disfruto mucho leer
+          blogs o foros sobre ello.
+          <br />
+          Mi filosofía de trabajo se centra en crear productos que no solo
+          funcionen bien, sino que también proporcionen una experiencia
+          exceptional al usuario. Creo firmemente en el aprendizaje continuo y
+          en campartir conocimiento con la comunidad.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
