3d1d38fcf7fe484ea8e77eb93526f3d4d96fd344
anthonyygpz@gmail.com <anthonyygpz@gmail.com>
2025-03-24 00:38:31 -0700

    refactor(main.tsx): cambio de agregacion se agrego aboutMe

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -3,11 +3,13 @@ import { Projects } from "../sections/projects";
 import { Skills } from "../sections/skills";
 import { TitleMain } from "../sections/titleMain";
 import { Contact } from "../sections/contact";
+import AboutMe from "../sections/aboutMe";
 
 const Main = () => {
   return (
     <Default>
       <TitleMain />
+      <AboutMe />
       <Skills />
       <Projects />
       <Contact />
