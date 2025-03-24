f6d14c2c007369727a4ec7ae4278ae9821dcbfa5
anthonyygpz@gmail.com <anthonyygpz@gmail.com>
2025-03-24 00:38:04 -0700

    refactor(useHeader.ts): cambio de agregacion se agrego el copiar el correo

--- src/hooks/useHeader.ts
+++ src/hooks/useHeader.ts
@@ -2,12 +2,45 @@ import { useState } from "react";
 
 const useHeader = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const [tooltipText, setTooltipText] = useState<string>("Copiar correo");
+  const [showTooltip, setShowTooltip] = useState<boolean>(false);
+  const email = "anthonyygpz@gmail.com";
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  return { toggleMenu, isMenuOpen, setIsMenuOpen };
+  const scrollSection = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const gmailCopyButton = () => {
+    navigator.clipboard
+      .writeText(email)
+      .then(() => {
+        setTooltipText("!Se ha copiado!");
+        setTimeout(() => {
+          setTooltipText("Copiar Correo");
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error("Error al copiar: ", error);
+      });
+  };
+
+  return {
+    toggleMenu,
+    isMenuOpen,
+    setIsMenuOpen,
+    scrollSection,
+    gmailCopyButton,
+    tooltipText,
+    showTooltip,
+    setShowTooltip,
+  };
 };
 
 export default useHeader;
