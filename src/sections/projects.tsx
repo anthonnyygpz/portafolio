17a10a80e73637d525176bc23de3d18029a7e719
anthonyygpz@gmail.com <anthonyygpz@gmail.com>
2025-03-24 00:44:09 -0700

    refactor(projects.tsx): cambio de agregacion se agrego el section y su id

--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -97,17 +97,19 @@ export const Projects = () => {
   ];
 
   return (
-    <div className="p-6">
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {projects.map((project) => (
-          <ProjectCard
-            key={project.id}
-            title={project.title}
-            languages={project.languages}
-            imageSrc={project.imageSrc}
-          />
-        ))}
+    <section id="projects">
+      <div className="p-6">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {projects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              languages={project.languages}
+              imageSrc={project.imageSrc}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
